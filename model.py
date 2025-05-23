af8ef0f0588601c5ef91404e6d80972a80503e62
sreenisha2005@gmail.com <sreenisha2005@gmail.com>
2025-05-23 19:09:20 +0530

    change 4

--- model.py
+++ model.py
@@ -9,19 +9,6 @@ path = kagglehub.dataset_download("jp797498e/twitter-entity-sentiment-analysis")
 
 print("Path to dataset files:", path)
 
-# Sample Data (Replace with your dataset)
-# data = {
-#     "text": [
-#         "I love this product!",
-#         "This is terrible.",
-#         "Awesome experience.",
-#         "Worst service ever.",
-#         "It's okay, not bad."
-#     ],
-#     "label": [1, 0, 1, 0, 0]  # 1=Positive, 0=Negative
-# }
-# df = pd.DataFrame(data)
-
 df = pd.read_csv(f"{path}/twitter_training.csv", header=None, names=['index', 'borderlands', 'label', 'text'])
 print(df.shape)
 print(df.head)
